0712e62bb2663a12b5484894c6a6f7bd997d975b
phuongnam97 <37271996+phuongnam97@users.noreply.github.com>
2018-08-15 13:52:25 +0700

    Done cart-collection migration seeder

---
+++ database/migrations/2018_08_15_061126_create_carts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->integer('cart_id');
+            $table->primary('cart_id');
+            $table->foreign('cart_id')->references('user_id')->on('accounts');
+            $table->string('cart_name');
+            $table->foreign('cart_name')->references('username')->on('accounts');
+            $table->timestamps();
+            $table->integer('status')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+}
