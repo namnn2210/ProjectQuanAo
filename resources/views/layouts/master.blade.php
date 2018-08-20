c1a1ccdf7f300ca8a5516926be6f5ac9e2d7348b
namngocngo22 <namngocngo22@gmail.com>
2018-08-21 00:02:24 +0700

    DONE Master Layout cực xịn

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,160 +1,215 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>@yield('page-title')</title>
-    <link href="{{asset('/css/masterLayout_css/bootstrap.min.css')}}" rel="stylesheet">
-    <link href="{{asset('/css/masterLayout_css/metisMenu.min.css')}}" rel="stylesheet">
-    <link href="{{asset('/css/masterLayout_css/sb-admin-2.css')}}" rel="stylesheet">
-    <link href="{{asset('/css/datatables-responsive/dataTables.responsive.css')}}" rel="stylesheet"/>
-    <link href="{{asset('/font/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css" rel="stylesheet"/>
-    <link rel="stylesheet" href="{{URL::asset('css/form.css')}}">
-    <link href="{{asset('/css/datatables/css/dataTables.bootstrap.css')}}" rel="stylesheet"/>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.7 -->
+    <link rel="stylesheet" href="{{asset('css/master-layout-2-css/bootstrap.min.css')}}">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" type="text/css" href="{{asset('/font/font-awesome/css/font-awesome.min.css')}}">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="{{asset('css/master-layout-2-css/ionicons.min.css')}}">
+    <!-- jvectormap -->
+    <link rel="stylesheet" href="{{asset('css/master-layout-2-css/jquery-jvectormap.css')}}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{asset('css/master-layout-2-css/AdminLTE.min.css')}}">
+    <!-- AdminLTE Skins. Choose a skin from the css/skins
+         folder instead of downloading all of them to reduce the load. -->
+    <link rel="stylesheet" href="{{asset('css/master-layout-2-css/_all-skins.min.css')}}">
+
+    <!-- Google Font -->
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
-<body>
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
 
-<div id="wrapper">
-    <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+    <header class="main-header">
 
-        <!--Thanh điều hướng ngang trên cùng-->
-        <div class="navbar-header">
-            <a class="navbar-brand" href="index.html">SB Admin v2.0</a>
-        </div>
-        <ul class="nav navbar-top-links navbar-right">
-            <li class="dropdown">
-                <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                    <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
-                </a>
-                <ul class="dropdown-menu dropdown-user">
+        <!-- Logo -->
+        <a href="index2.html" class="logo">
+            <!-- mini logo for sidebar mini 50x50 pixels -->
+            <span class="logo-mini"><b>F</b>FF</span>
+            <!-- logo for regular state and mobile devices -->
+            <span class="logo-lg"><b>Far</b>Fetch</span>
+        </a>
 
-                    <!--Dẫn tới trang thông tin một admin-->
-                    <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
-                    </li>
-                    <!--Dẫn tới trang thông tin một admin-->
+        <!-- Header Navbar: style can be found in header.less -->
+        <nav class="navbar navbar-static-top">
+            <!-- Sidebar toggle button-->
+            <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+                <span class="sr-only">Toggle navigation</span>
+            </a>
+            <!-- Navbar Right Menu -->
+            <div class="navbar-custom-menu">
+                <ul class="nav navbar-nav">
+                    <!-- User Account: style can be found in dropdown.less -->
+                    <li class="dropdown user user-menu">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            <img src="{{asset('img/icon-user-160x160.png')}}" class="user-image" alt="User Image">
+                            <span class="hidden-xs">Admin</span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <!-- User image -->
+                            <li class="user-header">
+                                <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
 
-                    <!--Dẫn tới trang cài đặt cho một admin-->
-                    <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
-                    </li>
-                    <!--Dẫn tới trang cài đặt cho một admin-->
-
-                    <li class="divider"></li>
-                    <li><a href="login.html"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
-                    </li>
-                </ul>
-            </li>
-        </ul>
-        <!--Thanh điều hướng ngang trên cùng-->
-
-        <div class="navbar-default sidebar" role="navigation">
-            <div class="sidebar-nav navbar-collapse">
-                <ul class="nav" id="side-menu">
-
-                    <!--Thanh tìm kiếm-->
-                    <li class="sidebar-search">
-                        <div class="input-group custom-search-form">
-                            <input type="text" class="form-control" placeholder="Search...">
-                            <span class="input-group-btn">
-                                <button class="btn btn-default" type="button">
-                                    <i class="fa fa-search"></i>
-                                </button>
-                            </span>
-                        </div>
-                    </li>
-                    <!--Thanh tìm kiếm-->
-
-                    <!--Menu dropdown cho từng chức năng-->
-                    <li>
-                        <a href="#"><i class="fa fa-wrench fa-fw"></i> Function 1<span class="fa arrow"></span></a>
-                        <ul class="nav nav-second-level">
-                            <li>
-                                <a href="#">Sub_Function 1</a>
+                                <p>
+                                    Admin - Web Administration
+                                    <small>Member since Aug. 2018</small>
+                                </p>
                             </li>
-                            <li>
-                                <a href="#">Sub_Function 2</a>
-                            </li>
-                            <li>
-                                <a href="#">Sub_Function 3</a>
-                            </li>
-                            <li>
-                                <a href="#">Sub_Function 4</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <!--Menu dropdown cho từng chức năng-->
-
-                    <li>
-                        <a href="#"><i class="fa fa-wrench fa-fw"></i> Function 2<span class="fa arrow"></span></a>
-                        <ul class="nav nav-second-level">
-                            <li>
-                                <a href="#">Sub_Function 1</a>
-                            </li>
-                            <li>
-                                <a href="#">Sub_Function 2</a>
-                            </li>
-                            <li>
-                                <a href="#">Sub_Function 3</a>
-                            </li>
-                            <li>
-                                <a href="#">Sub_Function 4</a>
-                            </li>
-                        </ul>
-                    </li>
-
-                    <li>
-                        <a href="#"><i class="fa fa-wrench fa-fw"></i> Function 3<span class="fa arrow"></span></a>
-                        <ul class="nav nav-second-level">
-                            <li>
-                                <a href="#">Sub_Function 1</a>
-                            </li>
-                            <li>
-                                <a href="#">Sub_Function 2</a>
-                            </li>
-                            <li>
-                                <a href="#">Sub_Function 3</a>
-                            </li>
-                            <li>
-                                <a href="#">Sub_Function 4</a>
+                            <!-- Menu Body -->
+                            <!-- Menu Footer-->
+                            <li class="user-footer">
+                                <div class="pull-left">
+                                    <a href="#" class="btn btn-default btn-flat">Profile</a>
+                                </div>
+                                <div class="pull-right">
+                                    <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                                </div>
                             </li>
                         </ul>
                     </li>
                 </ul>
             </div>
-        </div>
-    </nav>
-    <div id="page-wrapper">
+
+        </nav>
+    </header>
+    <!-- Left side column. contains the logo and sidebar -->
+    <aside class="main-sidebar">
+        <!-- sidebar: style can be found in sidebar.less -->
+        <section class="sidebar">
+            <!-- Sidebar user panel -->
+            <div class="user-panel">
+                <div class="pull-left image">
+                    <img src="{{asset('img/icon-user-160x160.png')}}" class="img-circle" alt="User Image">
+                </div>
+                <div class="pull-left info">
+                    <p>Admin</p>
+                    <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+                </div>
+            </div>
+            <!-- search form -->
+            <form action="#" method="get" class="sidebar-form">
+                <div class="input-group">
+                    <input type="text" name="q" class="form-control" placeholder="Search...">
+                    <span class="input-group-btn">
+                <button type="submit" name="search" id="search-btn" class="btn btn-flat">
+                  <i class="fa fa-search"></i>
+                </button>
+              </span>
+                </div>
+            </form>
+            <!-- /.search form -->
+            <!-- sidebar menu: : style can be found in sidebar.less -->
+            <ul class="sidebar-menu" data-widget="tree">
+                <li class="header">MAIN MANAGEMENT</li>
+                <li class="active treeview menu-open">
+                    <a href="#">
+                        <i class="fa fa-user"></i> <span>User Management</span>
+                        <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                    </a>
+                    <ul class="treeview-menu">
+                        <li><a href="#"><i class="fa fa-circle-o"></i>List of Users</a></li>
+                    </ul>
+                </li>
+                <li class="treeview">
+                    <a href="#">
+                        <i class="fa fa-product-hunt"></i>
+                        <span>Product Management</span>
+                        <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                    </a>
+                    <ul class="treeview-menu">
+                        <li><a href="/product"><i class="fa fa-circle-o"></i>List of Products</a></li>
+                        <li><a href="#"><i class="fa fa-circle-o"></i>Create a new product</a></li>
+                    </ul>
+                </li>
+                <li class="treeview">
+                    <a href="#">
+                        <i class="fa fa-align-justify"></i>
+                        <span>Category Management</span>
+                        <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                    </a>
+                    <ul class="treeview-menu">
+                        <li><a href="#"><i class="fa fa-circle-o"></i>List of Categories</a></li>
+                        <li><a href="#"><i class="fa fa-circle-o"></i>Create a new category</a></li>
+                    </ul>
+                </li>
+                <li class="treeview">
+                    <a href="#">
+                        <i class="fa fa-tag"></i> <span>Brand Management</span>
+                        <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                    </a>
+                    <ul class="treeview-menu">
+                        <li><a href="#"><i class="fa fa-circle-o"></i>List of Brands</a></li>
+                        <li><a href="#"><i class="fa fa-circle-o"></i>Create a new brand</a></li>
+                    </ul>
+                </li>
+                <li class="treeview">
+                    <a href="#">
+                        <i class="fa fa-archive"></i> <span>Collection Managment</span>
+                        <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                    </a>
+                    <ul class="treeview-menu">
+                        <li><a href="#"><i class="fa fa-circle-o"></i>List of Collections</a></li>
+                        <li><a href="#"><i class="fa fa-circle-o"></i>Create a new collection</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </section>
+        <!-- /.sidebar -->
+    </aside>
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
         @section('content')
         @show
     </div>
+    <!-- /.content-wrapper -->
+
+    <footer class="main-footer">
+        <div class="pull-right hidden-xs">
+            <b>Version</b> 2.4.0
+        </div>
+        <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+        reserved.
+    </footer>
+
 
 </div>
+<!-- ./wrapper -->
 
-<!-- jQuery -->
-<script src="{{asset('/js/masterLayout_js/jquery.min.js')}}"></script>
-
-<!-- Bootstrap Core JavaScript -->
-<script src="{{asset('/js/masterLayout_js/bootstrap.min.js')}}"></script>
-
-<!-- Metis Menu Plugin JavaScript -->
-<script src="{{asset('/js/masterLayout_js/metisMenu.min.js')}}"></script>
-
-<!-- Custom Theme JavaScript -->
-<script src="{{asset('/js/masterLayout_js/sb-admin-2.js')}}"></script>
-<script src="{{asset('/css/datatables/js/jquery.dataTables.min.js')}}"></script>
-<script src="{{asset('/datatables-plugins/dataTables.bootstrap.min.js')}}"></script>
-<script src="{{asset('/css/datatables-responsive/dataTables.responsive.css')}}"></script>
-<script>
-    $(document).ready(function() {
-        $('#dataTables-example').DataTable({
-            responsive: true
-        });
-    });
-</script>
+<!-- jQuery 3 -->
+<script src="{{asset('js/master-layout-2-js/jquery.min.js')}}"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="{{asset('js/master-layout-2-js/bootstrap.min.js')}}"></script>
+<!-- FastClick -->
+<script src="{{asset('js/master-layout-2-js/fastclick.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('js/master-layout-2-js/adminlte.min.js')}}"></script>
+<!-- Sparkline -->
+<script src="{{asset('js/master-layout-2-js/jquery.sparkline.min.js')}}"></script>
+<!-- jvectormap  -->
+<script src="{{asset('js/master-layout-2-js/jquery-jvectormap-1.2.2.min.js')}}"></script>
+<script src="{{asset('js/master-layout-2-js/jquery-jvectormap-world-mill-en.js')}}"></script>
+<!-- SlimScroll -->
+<script src="{{asset('js/master-layout-2-js/jquery.slimscroll.min.js')}}"></script>
+<!-- ChartJS -->
+<script src="{{asset('js/master-layout-2-js/Chart.js')}}"></script>
 </body>
 </html>
