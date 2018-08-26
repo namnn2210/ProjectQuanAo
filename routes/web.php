71dae2f28a959aab5272cb3adf7e40c14f4aa00c
phuongnam97 <37271996+phuongnam97@users.noreply.github.com>
2018-08-26 19:57:16 +0700

    Xong product detail

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,14 @@ Route::get('/admin/product/{id}/quickEdit','ProductController@quickEdit');
 Route::put('/admin/quickUpdate/product/','ProductController@quickUpdate');
 Route::get('/admin/category/{id}/quickEdit','CategoryController@quickEdit');
 Route::put('/admin/quickUpdate/category/','CategoryController@quickUpdate');
-Route::get('/admin/collection/{id}/quickEdit','CollectionController@quickEdit');
-Route::put('/admin/quickUpdate/collection/','CollectionController@quickUpdate');
+//Route::get('/admin/collection/{id}/quickEdit','CollectionController@quickEdit');
+//Route::put('/admin/quickUpdate/collection/','CollectionController@quickUpdate');
 Route::get('/admin/account/{id}/quickEdit','AccountController@quickEdit');
 Route::put('/admin/quickUpdate/account/','AccountController@quickUpdate');
 
 
 Route::resource('admin/product','ProductController');
 Route::resource('admin/category','CategoryController');
-Route::resource('admin/collection','CollectionController');
 Route::resource('admin/account','AccountController');
 
 Route::get('/register', function () {
@@ -32,13 +31,13 @@ Route::get('/login', function () {
     return view('admin.login');
 });
 Route::get('/', function () {
-   return view('layouts.master');
+    return view('layouts.master');
 });
 
 Route::get('/','HomepageController@showHomepage');
 
 Route::get('/product','UserProductController@index');
-Route::post('/san-pham-test','UserProductController@search');
-
-
-
+Route::get('/product/{product}','UserProductController@show');
+Route::get('/abc', function (){
+    return view('user.product-detail') ;
+});
