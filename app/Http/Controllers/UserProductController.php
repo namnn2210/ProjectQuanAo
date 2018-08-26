f35644c679e44221b9e88a4e5b96e1debb763e86
JUstSaLtPiLe <vu.tienphuc97@gmail.com>
2018-08-26 13:33:05 +0700

    fix lai gia viet

--- app/Http/Controllers/UserProductController.php
+++ app/Http/Controllers/UserProductController.php
@@ -38,24 +38,46 @@ class UserProductController
         $chosen_category = 0;
         if(Input::has('search') && Input::get('search') != null  && Input::has('sort') && Input::has('value1') && Input::has('value2')){
             $search = Input::get('search');
-            $obj = Product::where('name', 'LIKE', '%'.$search.'%')
-                ->whereBetween('price' , [Input::get('value1'), Input::get('value2')])
-                ->orderBy('price', Input::get('sort'))
-                ->get();
-        }
-        else{
-            if (Input::has('categoryId') && Input::get('categoryId') != 0 && Input::has('sort') && Input::has('value1') && Input::has('value2')){
-                $chosen_category = Input::get('categoryId');
-                $obj = Product::where('category_id', $chosen_category)
+            if(Input::get('sort') == 'none'){
+                $obj = Product::where('name', 'LIKE', '%'.$search.'%')
+                    ->whereBetween('price' , [Input::get('value1'), Input::get('value2')])
+                    ->get();
+            }
+            else{
+                $obj = Product::where('name', 'LIKE', '%'.$search.'%')
                     ->whereBetween('price' , [Input::get('value1'), Input::get('value2')])
                     ->orderBy('price', Input::get('sort'))
                     ->get();
             }
+        }
+        else{
+            if (Input::has('categoryId') && Input::get('categoryId') != 0 && Input::has('sort') && Input::has('value1') && Input::has('value2')){
+                $chosen_category = Input::get('categoryId');
+                if(Input::get('sort') == 'none'){
+                    $obj = Product::where('category_id', $chosen_category)
+                        ->whereBetween('price' , [Input::get('value1'), Input::get('value2')])
+                        ->get();
+                }
+                else{
+                    $obj = Product::where('category_id', $chosen_category)
+                        ->whereBetween('price' , [Input::get('value1'), Input::get('value2')])
+                        ->orderBy('price', Input::get('sort'))
+                        ->get();
+                }
+            }
             else if (Input::get('categoryId') == 0 && Input::has('sort') && Input::has('value1') && Input::has('value2')){
-                $obj = Product::where('status', 1)
-                    ->whereBetween('price' , [Input::get('value1'), Input::get('value2')])
-                    ->orderBy('price', Input::get('sort'))
-                    -> get();
+
+                if(Input::get('sort') == 'none'){
+                    $obj = Product::where('status', 1)
+                        ->whereBetween('price' , [Input::get('value1'), Input::get('value2')])
+                        -> get();
+                }
+                else{
+                    $obj = Product::where('status', 1)
+                        ->whereBetween('price' , [Input::get('value1'), Input::get('value2')])
+                        ->orderBy('price', Input::get('sort'))
+                        -> get();
+                }
             }
         }
         return response()->json(['obj' => $obj], 200);
