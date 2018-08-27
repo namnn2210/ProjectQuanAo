73bef8c240e9386f69451016805dd9e28e9cd8be
namngocngo22 <namngocngo22@gmail.com>
2018-08-28 01:49:24 +0700

    hòm hòm

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -26,4 +26,16 @@ class DashboardController
         $orders = Order::all()->where('created_at','<=',Carbon::now())->where('created_at','>=',Carbon::yesterday());
         return view('admin.order.new_order')->with('orders',$orders);
     }
+
+    public function getChartDataApi()
+    {
+        $start_date = '2018-08-20';
+        $end_date = '2018-08-25';
+        $chart_data = Order::select(DB::raw('sum(total_price) as revenue'), DB::raw('date(created_at) as day'))
+            ->whereBetween('created_at', array($start_date, $end_date))
+            ->groupBy('day')
+            ->orderBy('day', 'desc')
+            ->get();
+        return $chart_data;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ Route::get('/login', function () {
 });
 Route::get('/admin','DashboardController@showAdminPage');
 Route::get('/admin/new-orders','DashboardController@showNewOrder');
+Route::get('/admin/chart','DashboardController@getChartDataApi');
 
 Route::get('/','HomepageController@showHomepage');
 
