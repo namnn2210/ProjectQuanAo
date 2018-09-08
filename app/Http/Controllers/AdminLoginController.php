ffe6415d7e6925c66a96235eb0349f6125941047
Nam Gay <vohophuongnam@gmail.com>
2018-09-08 19:31:57 +0700

    xong phan dang ky co ban

--- app/Http/Controllers/AdminLoginController.php
+++ app/Http/Controllers/AdminLoginController.php
@@ -3,21 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Account;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Hash;
 
 class AdminLoginController extends Controller
 {
-    public function login(){
+    public function login()
+    {
         return view('admin.login');
     }
 
-    public function postLogin(Request $request){
+    public function postLogin(Request $request)
+    {
 //        return dd($request);
         $request->validate([
             'username' => 'required|min:6|max:20',
             'password' => 'required|min:6|max:20'
-        ],[
+        ], [
             'username.required' => 'Bạn phải nhập tên đăng nhập',
             'password.required' => 'Bạn phải nhập tên mật khẩu',
             'username.min' => 'Tên đăng nhập từ 6-20 ký tự',
@@ -26,34 +31,38 @@ class AdminLoginController extends Controller
             'password.max' => 'Mật khẩu từ 6-20 ký tự'
         ]);
 
-        if(Auth::attempt(['username'=>$request->username, 'password'=>$request->password])){
+        if (Auth::attempt(['username' => $request->username, 'password' => $request->password])) {
             return redirect('/admin/product');
+        } else {
+            return redirect()->back()->with('message', 'Tên đăng nhập hoặc mật khẩu không đúng');
         }
-        else {return redirect()->back()->with('message', 'Tên đăng nhập hoặc mật khẩu không đúng');}
     }
 
-    public function logout(){
+    public function logout()
+    {
         Auth::logout();
         return redirect('/admin/login');
     }
 
-    public function registerAdmin(Request $request){
+    public function registerAdmin(Request $request)
+    {
         return view('admin.register');
     }
 
-    public function createAdminAccount(Request $request){
+    public function createAdminAccount(Request $request)
+    {
         $request->validate([
             'username' => 'required|min:6|max:20',
             'password' => 'required|min:6|max:20',
             'email' => 'required|email',
             'confirm-password' => 'required|min:6|max:20|same:password',
-            'fullname' =>'required|min:4'
-        ],[
+            'full_name' => 'required|min:2'
+        ], [
             'username.required' => 'Bạn phải nhập tên đăng nhập',
             'password.required' => 'Bạn phải nhập tên mật khẩu',
             'confirm-password.required' => 'Bạn phải nhập tên mật khẩu',
             'email.required' => 'Bạn phải nhập email',
-            'fullname.required' => 'Bạn phải nhập tên đầy đủ của bạn',
+            'full_name.required' => 'Bạn phải nhập tên đầy đủ của bạn',
             'username.min' => 'Tên đăng nhập từ 6-20 ký tự',
             'username.max' => 'Tên đăng nhập từ 6-20 ký tự',
             'password.min' => 'Mật khẩu từ 6-20 ký tự',
@@ -62,11 +71,45 @@ class AdminLoginController extends Controller
             'confirm-password.max' => 'Mật khẩu từ 6-20 ký tự',
             'confirm-password.same' => 'Mật khẩu xác nhận phải giống với mật khẩu đã nhập',
             'email.email' => 'Email không đúng định dạng',
-            'fullname.min' => 'Tên phải có trên 4 kí tự'
+            'full_name.min' => 'Tên phải có trên 4 kí tự'
         ]);
 
-//        return redirect('/admin/login')->with('message','Đăng ký thành công, xin mời đăng nhập');
-        return redirect()->back()->with('message','Đăng ký không thành công');
+        if ($this->isExistUsername(Input::get('username'))) {
+            return redirect()->back()->with('message', 'Đăng ký không thành công, tên tài khoản đã tồn tại');
+        }
+        if ($this->isExistEmail(Input::get('email'))) {
+            return redirect()->back()->with('message', 'Đăng ký không thành công, email đã tồn tại');
+        }
+
+        $obj = new User();
+        $obj->username = Input::get('username');
+        $obj->password = hash::make(Input::get('password'));
+        $obj->full_name = Input::get('full_name');
+        $obj->email = Input::get('email');
+        $obj->address = Input::get('address');
+        $obj->gender = str_replace('option','',Input::get('gender'));
+        $obj->phone = Input::get('phone');
+        $obj->DOB = Input::get('DOB');
+        $obj->role = 1;
+        $obj->status = 0;
+        $obj->save();
+        return redirect()->back()->with('message', 'Đăng ký thành công');
+    }
+
+    public static function isExistEmail($email)
+    {
+        $obj = User::where('email', '=', $email)->first();
+        if ($obj != null) {
+            return true;
+        } else return false;
+    }
+
+    public static function isExistUsername($username)
+    {
+        $obj = User::where('email', '=', $username)->first();
+        if ($obj != null) {
+            return true;
+        } else return false;
     }
 
 }
