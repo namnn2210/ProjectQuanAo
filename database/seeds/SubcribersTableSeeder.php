3bd97d388f93e40604038a0671993b790f7ee43b
Nam Gay <vohophuongnam@gmail.com>
2018-09-11 13:47:26 +0700

    seeder subcriber

--- database/seeds/SubcribersTableSeeder.php
+++ database/seeds/SubcribersTableSeeder.php
@@ -11,6 +11,27 @@ class SubcribersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        \Illuminate\Support\Facades\DB::table('subscribeds')->truncate();
+        \Illuminate\Support\Facades\DB::table('subscribeds')->insert([
+            [
+                'email'=>'vohophuongnam@gmail.com',
+                'name'=>'Vo Ho Phuong Nam',
+                'address'=>'Hanoi',
+                'phone'=>'01232301997',
+                'status'=>1,
+            ],[
+                'email'=>'ngongocnam221097@gmail.com',
+                'name'=>'Ngo Ngoc Nam',
+                'address'=>'Hanoi',
+                'phone'=>'01678884544',
+                'status'=>1,
+            ],[
+                'email'=>'hahoanglc97@gmail.com',
+                'name'=>'Hoang Ha <3 Nam Gay',
+                'address'=>'Hanoi',
+                'phone'=>'01659632564',
+                'status'=>1,
+            ],
+        ]);
     }
 }
