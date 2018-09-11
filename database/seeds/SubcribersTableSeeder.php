035483c99e65aec3b9e3a6a9b8d19b4435cf629c
namngocngo22 <namngocngo22@gmail.com>
2018-09-11 13:29:40 +0700

    thêm seeder, config mail

---
+++ database/seeds/SubcribersTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SubcribersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
